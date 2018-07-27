39a212e9487014c470fd8f014d573f6ce9e078a8
Yamashta Daisuke <yamasdais@gmail.com>
2018-07-27 22:35:27 +0900

    added serialization sample.

--- c++/hana0.cxx
+++ c++/hana0.cxx
@@ -1,7 +1,12 @@
 #include <cassert>
 #include <string>
 #include <iostream>
+#include <array>
+#include <typeinfo>
+#include <typeindex>
+#include <utility>
 
+#include <boost/any.hpp>
 #include <boost/hana.hpp>
 
 #include "util.hpp"
@@ -30,7 +35,7 @@ void test0() {
     Dog{"Hati"}
   );
   // 1_c はユーザ定義リテラルで、コンパイル時定数
-  Cat dr_evils_pet = animals[1_c];
+  const Cat dr_evils_pet = animals[1_c];
 
   std::cout << "Dr. Evil's pet:" << dr_evils_pet.name << std::endl;
 
@@ -74,10 +79,88 @@ void test0() {
   std::cout << "lambda returns: "
             << T_name(hana::traits::is_pointer(types[0_c])) << std::endl;
   // => boost::hana::integral_constant<bool, true>
+
+  auto has_name = hana::is_valid([](auto&& x) -> decltype((void)x.name) {});
+  static_assert(has_name(dr_evils_pet));
+  static_assert(!has_name(1));
 }
+
+struct Person {
+  BOOST_HANA_DEFINE_STRUCT(
+    Person,
+    (std::string, name),
+    (int, age)
+  );
+};
+
+void test1() {
+  constexpr auto serialize = [](std::ostream& os, auto const& object) {
+    hana::for_each(hana::members(object), [](const auto& member) {
+        std::cout << T_name(member) << std::end;
+      });
+  };
+
+  const Person john{"John", 30};
+  auto m = hana::members(john);
+  std::cout << "members(john): " << T_name(m) << std::endl;
+  hana::for_each(m, [](auto member) {
+      std::cout << "foreach:" << member << std::endl;
+    });
+//  serialize(std::cout, john);
+}
+
+// switch sample
+//[case]
+#if false
+template <typename T>
+auto case_ = [](auto f) {
+  return hana::make_pair(hana::type_c<T>, f);
+};
+
+struct default_t;
+auto default_ = case_<default_t>;
+
+// [process]
+template <typename Any, typename Default>
+auto process(Any&, std::type_index const& t, Default& default_) {
+  return default_();
+}
+
+template <typename Any, typename Default, typename Case, typename... Rest>
+auto process(Any& a, std::type_index const& t, Default& default_,
+             Case& case_, Rest& ...rest)
+{
+  using T = typename decltype(+hana::first(case_))::type;
+  return t == typeid(T)
+  ? hana::second(case_)(*boost::unsafe_any_cast<T>(&a))
+  : process(a, t, default_, rest...);
+}
+// [switch]
+template <typename Any>
+auto switch_(Any& a) {
+  return [&a](auto ...cases_) {
+    auto cases = hana::make_tuple(cases_...);
+
+    auto default_ = hana::find_if(cases, [](auto const& c) {
+        return hana::first(c) == hana::type_c<default_t>;
+      });
+    static_assert(default_ != hana::nothing,
+                  "switch is missing a default_ case");
+
+    auto rest = hana::filter(cases, [](auto const& c) {
+        return hana::first(c) != hana::type_c<default_t>;
+      });
+
+    return hana::unpack(rest, [&](auto& ...rest) {
+        return process(a, a.type(), hana::second(*default_), rest...);
+      });
+  };
+}
+#endif
 
 int main(int, char**) {
   test0();
+  test1();
   
   return 0;
 }
