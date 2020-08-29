c6790c7aaf19da0d5f3f4c05efb5e1496e777bf6
Yamashta, Daisuke <yamasdais@gmail.com>
2020-08-29 12:21:38 +0900

    Change to use primes cache

--- cpc/chg005.cxx
+++ cpc/chg005.cxx
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <ranges>
+#include <vector>
 #include "challenge.h"
 
 template <std::integral Num>
 bool check_sexy_primes(Num base, int count) {
+    auto primes = std::vector<Num>{};
+    primes.reserve(count);
     auto nums = std::ranges::iota_view{0, count}
-                | std::views::transform([base](const auto n) { return base + n * 6; });
+                | std::views::transform([base](auto const n) { return base + n * 6; });
+    // std::ranges::copy(nums, std::back_inserter(primes));
     if (std::all_of(std::ranges::cbegin(nums), std::ranges::cend(nums),
-                    [](auto v) { return cpc::is_prime(v); })) {
+                    [&primes](auto const v) {
+                        if (cpc::is_prime(v)) {
+                            primes.push_back(v);
+                            return true;
+                        }
+                        return false;
+                     })) {
         std::cout << "sexy prime numbers: ";
-        for (auto i : nums) {
+        for (auto i : primes) {
             std::cout << i << " ";
         }
         std::cout << std::endl;
     }
+
     return true;
 }
 
