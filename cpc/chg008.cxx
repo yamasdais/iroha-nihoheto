618126bf000515b95ead044769dc6c863a65f7d7
Yamashta, Daisuke <yamasdais@gmail.com>
2020-09-03 21:47:39 +0900

    Change some simple functions to expressions

--- cpc/chg008.cxx
+++ cpc/chg008.cxx
@@ -7,45 +7,27 @@
 template <size_t width>
 using ValArray = std::array<unsigned long, width>;
 
-auto makeIota(bool isFirst) {
-    return std::ranges::iota_view(isFirst ? 1u : 0u, 10u);
-}
-
-template <class Iter, class T>
-    requires std::random_access_iterator<Iter>&&
-        std::assignable_from<std::iter_reference_t<Iter>, T>
-auto makeValue(size_t index, T value, Iter ptr) {
-    ptr[index] = value;
-}
-
 template <class Iter>
-auto printValue(Iter const b, Iter const e) {
-    std::for_each(b, e, [](auto const n) { std::cout << n; });
-    std::cout << ",";
-}
-
-template <class Iter>
-requires std::sized_sentinel_for<Iter, Iter>
-auto calcArmstrong(Iter const b, Iter const e) {
-    auto size = std::distance(b, e);
-    return std::accumulate(b, e, 0,
-        [size](auto acc, auto i) { return acc + std::pow(i, size); });
-}
-
-template <class Iter>
+    requires std::random_access_iterator<Iter>
+    && std::assignable_from<std::iter_reference_t<Iter>, std::iter_value_t<Iter>>
+    && std::sized_sentinel_for<Iter, Iter>
 void accumNums(Iter s, Iter e, size_t index) {
-    auto iter = makeIota(index == 0);
-    auto remain = std::distance(s, e) - index;
+    auto iter = std::ranges::iota_view(index == 0 ? 1u : 0u, 10u);
+    auto size = std::distance(s, e);
+    auto remain = size - index;
     for (auto i : iter) {
-        makeValue(index, i, s);
+        s[index] = i;
         if (remain > 1) {
             accumNums(s, e, index + 1);
         } else {
-            auto sum = std::accumulate(s, e, 0, [](auto acc, auto n) { return acc * 10 + n; });
-            auto arm = calcArmstrong(s, e);
+            auto sum = std::accumulate(s, e, 0,
+                    [](auto acc, auto n) { return acc * 10 + n; });
+            auto arm = std::accumulate(s, e, 0,
+                    [size](auto acc, auto i) { return acc + std::pow(i, size); });
             if (sum == arm) {
                 // std::cout << "[" << sum << "," << arm << "]";
-                printValue(s, e);
+                std::for_each(s, e, [](auto const n) { std::cout << n; });
+                std::cout << ",";
             }
         }
     }
@@ -54,7 +36,6 @@ void accumNums(Iter s, Iter e, size_t index) {
 int main(int, char**) {
     auto constexpr width = 4;
     auto ary = ValArray<width>();
-    //std::cout << std::boolalpha;
     accumNums(std::ranges::begin(ary), std::ranges::end(ary), 0);
     std::cout << std::endl;
     return 0;
