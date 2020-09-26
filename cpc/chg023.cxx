695f8f1b4d15295c3065234df9ed81ff430b8095
Yamashta, Daisuke <yamasdais@gmail.com>
2020-09-26 19:48:51 +0900

    chg023 minor modify

--- cpc/chg023.cxx
+++ cpc/chg023.cxx
@@ -10,11 +10,13 @@ struct bytes_to_hexstr_fn {
     std::string operator()(IStart begin, IEnd end,
                            bool const uppercase = false) const {
         std::ostringstream oss;
-        if (uppercase) oss << std::uppercase; // = oss.setf(std::ios_base::uppercase)
-        for (; begin != end; ++begin) {
+        if (uppercase)
+            oss << std::uppercase;  // = oss.setf(std::ios_base::uppercase)
+
+        std::ranges::for_each(begin, end, [&oss](auto v) {
             oss << std::hex << std::setw(2) << std::setfill('0')
-                << static_cast<int>(*begin);
-        }
+                << static_cast<int>(v);
+        });
         return oss.str();
     }
 
@@ -28,15 +30,18 @@ struct bytes_to_hexstr_fn {
 inline constexpr bytes_to_hexstr_fn bytes_to_hexstr = {};
 
 template <std::ranges::forward_range Range>
-void print_range(Range&& range, char const* prefix, bool const uppercase = false) {
-    std::cout << prefix << bytes_to_hexstr(std::forward<Range>(range), uppercase) << std::endl;
+void print_range(Range&& range, char const* prefix,
+                 bool const uppercase = false) {
+    std::cout << prefix
+              << bytes_to_hexstr(std::forward<Range>(range), uppercase)
+              << std::endl;
 }
 
 int main(int, char**) {
     std::vector<unsigned char> const v{0xba, 0xad, 0xf0, 0x0d};
-    std::array<unsigned char, 6> const a {{ 0, 1, 2, 3, 4, 5 }};
-    unsigned char buf[5] = { 0x11, 0x22, 0x33, 0x44, 0x55 };
-    //std::cout << "v: " << bytes_to_hexstr(v) << std::endl;
+    std::array<unsigned char, 6> const a{{0, 1, 2, 3, 4, 5}};
+    unsigned char buf[5] = {0x11, 0x22, 0x33, 0x44, 0x55};
+    // std::cout << "v: " << bytes_to_hexstr(v) << std::endl;
     print_range(v, "v: ", true);
     print_range(a, "a: ", true);
     print_range(buf, "buf: ", true);
