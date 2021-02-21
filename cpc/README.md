87200862723fd742ead259eaa0b64e0f03f8eba5
Yamashta, Daisuke <yamasdais@gmail.com>
2021-02-21 09:58:48 +0900

    Add chg070

--- cpc/README.md
+++ cpc/README.md
@@ -105,7 +105,7 @@ __デザインパターン__
 |[chg067.cxx](src/chg067.cxx)|パスワードの検証|Decorator pattern。
 |[chg068.cxx](src/chg068.cxx)|パスワード生成|Composite pattern。非型template に string_view は渡せないが、lambda を渡せば意図を汲める事に気づいた。
 |[chg069.cxx](src/chg069.cxx)|社会保障番号生成|Template method pattern. 基底クラスポインタのコンテナの代わりに、lambda を std::function にしておけば、CRTP でも実装可能だと気がついた。
-|[chg070.cxx](src/chg070.cxx)|
+|[chg070.cxx](src/chg070.cxx)|承認処理|Chain of Responsibility pattern. std::function は class template を纏めて扱う場面で有効と云えそうだ。
 |[chg071.cxx](src/chg071.cxx)|
 |[chg072.cxx](src/chg072.cxx)|
 |[chg073.cxx](src/chg073.cxx)|
---
+++ cpc/src/chg070.cxx
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <optional>
+#include <concepts>
+
+#include "challenge.h"
+
+template <auto limit>
+    requires std::is_invocable_r_v<double, decltype(limit)>
+struct role {
+    constexpr bool can_approve(double amount) {
+        constexpr double lim_amt = std::invoke(limit);
+        return amount <= lim_amt;
+    }
+};
+
+struct expense {
+    double const amount;
+    std::string const description;
+};
+
+template <class Role>
+std::function<bool(double)> make_approvable_predicate() {
+    return [](double amount) noexcept {
+        return Role{}.can_approve(amount);
+    };
+}
+
+struct employee {
+    template <class Role>
+    employee(std::string_view name, Role, employee const* direct_manager = nullptr)
+        : name{name}, can_approve{make_approvable_predicate<Role>()}, direct_manager{direct_manager}
+    {}
+
+    template <class Role>
+    void set_role()
+    { can_approve = make_approvable_predicate<Role>(); }
+
+    void set_direct_manager(employee const* manager)
+    { direct_manager = manager; }
+
+    bool approve(expense const& exp) const {
+        if (can_approve(exp.amount)) {
+            std::cout << name << " approved expense '" << exp.description
+                << "', cost=" << exp.amount << std::endl;
+            return true;
+        } else if (direct_manager != nullptr) {
+            return direct_manager->approve(exp);
+        }
+        return true;
+    }
+
+  private:
+    std::string const name;
+    std::function<bool(double)> can_approve;
+    employee const* direct_manager;
+};
+
+using employee_role = role<[]() noexcept { return 1000.0; }>;
+using team_manager_role = role<[]() noexcept { return 10000.0; }>;
+using department_manager_role = role<[]() noexcept { return 100000.0; }>;
+using president_role = role<[]() noexcept { return std::numeric_limits<double>::max(); }>;
+
+void test0() {
+    auto cecil = employee("cecil williamson", president_role{});
+    auto david = employee("david jones", department_manager_role{}, &cecil);
+    auto robert = employee("robert booth", team_manager_role{}, &david);
+    auto john = employee("john smith", employee_role{}, &robert);
+
+    john.approve({ 500, "manazines"});
+    john.approve({ 5000, "hotel accomodation"});
+    john.approve({ 50000, "conference costs"});
+    john.approve({ 500000, "new lorry"});
+}
+
+int main(int, char**) {
+    test0();
+
+    return 0;
+}
